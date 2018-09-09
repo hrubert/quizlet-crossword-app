ef3542d093e1de3ad9a691dba50127930045a4d9
Tracy Herbert <tracynherbert@gmail.com>
2018-09-09 09:47:12 -0500

    added start script

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 const path = require('path')
+const fetch = require('node-fetch')
 const PORT = process.env.PORT || 3000
 
 // app.use(function(req, res, next) {
@@ -13,6 +14,10 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 
 app.use(express.static('public'));
+
+fetch('https://api.quizlet.com/2.0/sets/152676396?client_id=GRfAXGKv6t')
+    .then(res => res.json())
+    .then(json => console.log(json));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/index.html'));
